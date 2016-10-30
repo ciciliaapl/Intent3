3cb51a1bf5fa8f297e489d6b2d127b66f490a6a1
Cicilia <cicilia_putri_23rpl@student.smktelkom-mlg.sch.id>
2016-10-30 21:43:11 +0700

    Intent Telepon

--- app/src/main/java/id/sch/smktelkom_mlg/learn/intent3/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/learn/intent3/MainActivity.java
@@ -1,7 +1,10 @@
 package id.sch.smktelkom_mlg.learn.intent3;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +14,19 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
+        findViewById(R.id.ImageViewPhone).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dialPhoneNumber("0341712500");
+            }
+        });
+    }
+
+    private void dialPhoneNumber(String phoneNumber)
+    {
+        Intent intent = new Intent(Intent.ACTION_CALL);
+        intent.setData(Uri.parse("tel:" + phoneNumber));
+        if (intent.resolveActivity(getPackageManager()) != null)
+            startActivity(intent);
     }
 }
